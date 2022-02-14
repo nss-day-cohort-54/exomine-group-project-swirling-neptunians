3395f15858ef3bc446de79bf8bb98e915f6e1b46
Hilary Shaner <hilaryshaner@gmail.com>
2022-02-14 15:51:22 -0600

    added dropdown list of facilities

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 // Import Exomine
-import { Governors } from "./Governors.js";
+// import { Governors } from "./Governors.js";
+import { Facilities } from "./Facilities.js";
 
 // query selector to container id in index.html
 const mainContainer = document.querySelector("#container")
-mainContainer.innerHTML = Governors()
+mainContainer.innerHTML = Facilities()
+
 // function that renders Exomine into html
 
 // invoke function that renders Exomine into html
