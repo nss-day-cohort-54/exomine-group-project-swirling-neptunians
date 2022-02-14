751d8dcbd38fa049b263e07d6e4ed80feb52a7d2
Hilary Shaner <hilaryshaner@gmail.com>
2022-02-14 10:37:38 -0600

    added minerals

--- scripts/database.js
+++ scripts/database.js
@@ -1,5 +1,33 @@
 const database = {
-    transientState: {}
+    transientState: {},
+    //added an array of minerals
+    minerals: [
+        {
+            id: 1,
+            name: "iron",
+            weight: 72
+        },
+        {
+            id: 2,
+            name: "copper",
+            weight: 16
+        },
+        {
+            id: 3,
+            name: "magnesium",
+            weight: 25
+        },
+        {
+            id: 4,
+            name: "cobalt",
+            weight: 100
+        },
+        {
+            id: 5,
+            name: "zinc",
+            weight: 8
+        }
+    ]
 }
 
 export const setFacility = (facilityId) => {
@@ -13,8 +41,7 @@ export const getFacilities = () => {
 
 export const purchaseMineral = () => {
 
-        // Broadcast custom event to entire documement so that the
-        // application can re-render and update state
-        document.dispatchEvent( new CustomEvent("stateChanged") )
-    }
+    // Broadcast custom event to entire document so that the
+    // application can re-render and update state
+    document.dispatchEvent( new CustomEvent("stateChanged") )
 }
